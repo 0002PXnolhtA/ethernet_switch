5a432b2074fa9cc06627cf12ca5e84ebca18ce4c
0002PXnolhtA <3180103322@zju.edu.cn>
2023-07-24 09:43:07 +0800

    broadcast storm mitigation

--- rtl/mac/gmii/mac_r_gmii_tte_v3.v
+++ rtl/mac/gmii/mac_r_gmii_tte_v3.v
@@ -78,6 +78,8 @@ parameter   LLDP_DBG_MAC        =   48'h60BEB403060E;
 parameter   LLDP_DBG_PORT       =   16'h1;
 parameter   LLDP_DBG_SPEED      =   2'b11;
 parameter   LLDP_DBG_MODE       =   4'h1;
+parameter   BCS_PROT_TIMER      =   625000;
+parameter   BCS_PROT_QUOTA      =   3072;
 
 reg [ 2:0] conf_state, conf_state_next;
 reg [ 1:0] conf_valid_buf;
@@ -652,10 +654,10 @@ wire    [12:0]  ram_cnt_be;
 reg     [7:0]	data_fifo_din_reg;
 reg             data_fifo_wr;
 reg             data_fifo_wr_reg;
-wire            data_fifo_wr_dv;
+(*MARK_DEBUG="true"*) wire            data_fifo_wr_dv;
 wire    [11:0]  data_fifo_depth;
-reg     [19:0]  ptr_fifo_din;
-reg             ptr_fifo_wr;
+(*MARK_DEBUG="true"*)  reg     [19:0]  ptr_fifo_din;
+(*MARK_DEBUG="true"*)  reg             ptr_fifo_wr;
 wire            ptr_fifo_full;
 
 reg [ 4:0] lldp_state, lldp_state_next;
@@ -668,6 +670,12 @@ reg [ 3:0] lldp_mode;
 reg [23:0] lldp_cksm;
 reg [15:0] lldp_cksm_1;
 reg        lldp_sel;
+
+(*MARK_DEBUG="true"*) reg     [ 5:0]  bcs_prot_cnt_bc;
+reg     [27:0]  bcs_prot_cnt;
+// reg     [15:0]  bcs_prot_quota;
+(*MARK_DEBUG="true"*) reg     [15:0]  bcs_prot_byte_cnt;
+(*MARK_DEBUG="true"*) reg             bcs_prot_block;
 
 assign  ram_cnt_be = speed[1]?ram_nibble_be:{1'b0,ram_nibble_be[12:1]};
 assign  data_fifo_wr_dv = data_fifo_wr_reg & (ram_nibble_be[0] | speed[1]); 
@@ -740,10 +748,12 @@ always @(posedge rx_clk  or negedge rstn_mac)
             // if(mac_conf_reg[0] && ram_cnt_be<66) ptr_fifo_din[14]<=#DELAY 1;
             // else if(!mac_conf_reg[0] && ram_cnt_be<65) ptr_fifo_din[14]<=#DELAY 1;
             if(ram_cnt_be<65) ptr_fifo_din[14]<=#DELAY 1;
+            else if (bcs_prot_block && bcs_prot_cnt_bc == 'h6 && lldp_state[0]) ptr_fifo_din[14]<=#DELAY 1;
             else ptr_fifo_din[14]<=#DELAY 0;
             // if(mac_conf_reg[0] && ram_cnt_be>1520) ptr_fifo_din[15]<=#DELAY 1;
             // else if(!mac_conf_reg[0] && ram_cnt_be>1519) ptr_fifo_din[15]<=#DELAY 1;
             if(ram_cnt_be>MTU+20) ptr_fifo_din[15]<=#DELAY 1;
+            else if (bcs_prot_block && bcs_prot_cnt_bc == 'h6 && lldp_state[0]) ptr_fifo_din[15]<=#DELAY 1;
             else ptr_fifo_din[15]<=#DELAY 0;
             if(crc_result==CRC_RESULT_VALUE) ptr_fifo_din[13]<=#DELAY 1'b0;
             else ptr_fifo_din[13]<=#DELAY 1'b1;
@@ -764,14 +774,62 @@ always @(posedge rx_clk  or negedge rstn_mac)
         endcase
         end
 
+always @(posedge rx_clk or negedge rstn_mac) begin
+    if (!rstn_mac) begin
+        bcs_prot_cnt        <=  'b0;
+        bcs_prot_cnt_bc     <=  'b0;
+        // bcs_prot_quota      <=  BCS_PROT_QUOTA;
+        bcs_prot_byte_cnt   <=  'b0;
+        bcs_prot_block      <=  'b0;
+    end
+    else begin
+        if (bcs_prot_cnt == BCS_PROT_TIMER - 1) begin
+            bcs_prot_cnt        <=  'b0;
+        end
+        else begin
+            bcs_prot_cnt        <=  bcs_prot_cnt + 1'b1;
+        end
+        if (be_state != 0) begin
+            if (speed[1]) begin
+                if (ram_cnt_be > 1 && ram_cnt_be < 8) begin
+                    bcs_prot_cnt_bc <=  (data_ram_dout == 8'hFF) ? bcs_prot_cnt_bc + 1'b1 : bcs_prot_cnt_bc;
+                end
+            end
+            else begin
+                if (!ram_nibble_be[0] && ram_cnt_be > 0 && ram_cnt_be < 7) begin
+                    bcs_prot_cnt_bc <=  (data_ram_dout == 8'hFF) ? bcs_prot_cnt_bc + 1'b1 : bcs_prot_cnt_bc;
+                end
+            end
+        end
+        else begin
+            bcs_prot_cnt_bc <=  'b0;
+        end
+        // if (bcs_prot_cnt == BCS_PROT_TIMER - 1) begin
+        //     bcs_prot_quota      <=  BCS_PROT_QUOTA;
+        // end
+        if (bcs_prot_cnt == BCS_PROT_TIMER - 1) begin
+            bcs_prot_byte_cnt   <=  'b0;
+        end
+        else if (ptr_fifo_wr && ~|(ptr_fifo_din[15:13]) && bcs_prot_cnt_bc == 'h6 && lldp_state[0]) begin
+            bcs_prot_byte_cnt   <=  bcs_prot_byte_cnt + ptr_fifo_din[11:0];
+        end
+        if (bcs_prot_cnt == BCS_PROT_TIMER - 1) begin
+            bcs_prot_block      <=  'b0;
+        end
+        else if (mac_conf_reg[1] && bcs_prot_byte_cnt >= BCS_PROT_QUOTA) begin
+            bcs_prot_block      <=  'b1;
+        end
+    end
+end
+
 //============================================  
 //PTP rx_state.   
 //============================================ 
-reg     [7:0]   ptp_data; 
-reg             ptp_sel;
-// reg     [5:0]   ptp_state;
-reg     [31:0]  counter_ns_reg;
-reg     [63:0]  ptp_message_pad;
+    reg     [7:0]   ptp_data; 
+    reg             ptp_sel;
+    // reg     [5:0]   ptp_state;
+    reg     [31:0]  counter_ns_reg;
+    reg     [63:0]  ptp_message_pad;
 
     reg     [31:0]  ptp_time_now;   // timestamp of now
     reg     [31:0]  ptp_time_now_sys;   // timestamp of now, system side
@@ -1703,7 +1761,7 @@ always @(posedge rx_clk  or negedge rstn_mac)
         endcase
         end
 
-wire    [7:0]   data_fifo_din;
+(*MARK_DEBUG="true"*) wire    [7:0]   data_fifo_din;
 
 assign  data_ram_addrb = ram_cnt_be[10:0] | ram_cnt_tte[10:0] ;
 assign  calc = data_fifo_wr_dv | tte_fifo_wr_dv;
@@ -1865,7 +1923,9 @@ always @(posedge rx_clk or negedge rstn_mac) begin
         end
         else if (mgnt_state[1] && ptr_fifo_wr) begin
             mgnt_cnt        <=  ptr_fifo_din[11:0];
-            mgnt_flag[6:4]  <=  ptr_fifo_din[15:13];
+            mgnt_flag[6]    <=  (ptr_fifo_din[15:14] == 2'b10);
+            mgnt_flag[5]    <=  (ptr_fifo_din[15:14] == 2'b01);
+            mgnt_flag[4]    <=  ptr_fifo_din[15:13];
         end
         else if (mgnt_state[2] && tteptr_fifo_wr) begin
             mgnt_cnt        <=  tteptr_fifo_din[11:0];
